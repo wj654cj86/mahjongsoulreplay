86ead7f9bd4b551b3eb4b279cf9aca2ccfd9d042
WilliamTu <wj654cj86@gmail.com>
2024-06-27 22:34:21 +0800

    V1.0.1 禁止調整textarea的大小 如果格式錯誤輸出等於輸入

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let base2 = Object.fromEntries([...base].map((v, i) => ([v, i])));
 decodebutton.onclick = () => {
 	decodingText.value = encodingText.value.split('\n').map(url => {
 		let tmp = url.split('=');
-		if (tmp.length < 2) return '';
+		if (tmp.length < 2) return url;
 		let tmp2 = tmp[1].split('_');
-		if (tmp2.length < 3) return '';
+		if (tmp2.length < 3) return url;
 		return [tmp[0], '=', ...tmp2[0].split('').map((v, i) => {
 			if (v == '-') return '-';
 			let n = base2[v] - 17 - i;
@@ -19,9 +19,9 @@ decodebutton.onclick = () => {
 encodebutton.onclick = () => {
 	encodingText.value = decodingText.value.split('\n').map(url => {
 		let tmp = url.split('=');
-		if (tmp.length < 2) return '';
+		if (tmp.length < 2) return url;
 		let tmp2 = tmp[1].split('_');
-		if (tmp2.length < 2) return '';
+		if (tmp2.length < 2) return url;
 		return [tmp[0], '=', ...tmp2[0].split('').map((v, i) => {
 			if (v == '-') return '-';
 			let n = base2[v] + 17 + i;
