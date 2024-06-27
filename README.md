01dca145debe3ba513c5fa4688a9d93ee807639a
WilliamTu <wj654cj86@gmail.com>
2024-06-27 21:43:59 +0800

    V1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# 雀魂牌譜網址編碼解碼
+
+網址：https://wj654cj86.github.io/mahjongsoulreplay/
---
+++ main.js
@@ -0,0 +1,32 @@
+let base = '0123456789abcdefghijklmnopqrstuvwxyz';
+let base2 = Object.fromEntries([...base].map((v, i) => ([v, i])));
+
+decodebutton.onclick = () => {
+	decodingText.value = encodingText.value.split('\n').map(url => {
+		let tmp = url.split('=');
+		if (tmp.length < 2) return '';
+		let tmp2 = tmp[1].split('_');
+		if (tmp2.length < 3) return '';
+		return [tmp[0], '=', ...tmp2[0].split('').map((v, i) => {
+			if (v == '-') return '-';
+			let n = base2[v] - 17 - i;
+			while (n < 0) n += 36;
+			return base[n];
+		}), '_', tmp2[1]].join('');
+	}).join('\n');
+};
+
+encodebutton.onclick = () => {
+	encodingText.value = decodingText.value.split('\n').map(url => {
+		let tmp = url.split('=');
+		if (tmp.length < 2) return '';
+		let tmp2 = tmp[1].split('_');
+		if (tmp2.length < 2) return '';
+		return [tmp[0], '=', ...tmp2[0].split('').map((v, i) => {
+			if (v == '-') return '-';
+			let n = base2[v] + 17 + i;
+			while (n >= 36) n -= 36;
+			return base[n];
+		}), '_', tmp2[1], '_2'].join('');
+	}).join('\n');
+};
